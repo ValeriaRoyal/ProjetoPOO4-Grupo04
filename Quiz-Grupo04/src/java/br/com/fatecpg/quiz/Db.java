2f39869497836ebb08c8ab7ab45f421bb31a9dac
ValeriaRoyal <ValeriaRoyal@users.noreply.github.com>
2018-11-09 00:11:51 -0200

    ajuste nas perguntas e finalização do menu.

--- Quiz-Grupo04/src/java/br/com/fatecpg/quiz/Db.java
+++ Quiz-Grupo04/src/java/br/com/fatecpg/quiz/Db.java
@@ -17,16 +17,16 @@ public class Db {
     
     public static ArrayList<Quiz> getQuiz(){
         ArrayList<Quiz> quiz = new ArrayList<>();
-        quiz.add(new Quiz("Qual cavaleiro ajudou Athena a escapar do Santuário há 13 anos?", "Sagitário", new String[]{" Sagitário"," Libra"," Aries"}));
-        quiz.add(new Quiz("Qual Cavaleiro de Ouro é o mais belo?", "Afrodite de Peixes", new String[]{" Shura de Capricórnio"," Afrodite de Peixes"," Aldebaran de Touro"}));
-        quiz.add(new Quiz("Quem é o Cavaleiro de Ouro da Casa de Leão?", "Aioria", new String[]{" Aioria"," Miro"," Camus"}));
-        quiz.add(new Quiz("Cólera do Dragão é um grande golpe de qual Cavaleiro?", " Shiryu", new String[]{" Dohko"," Shion"," Shiryu"}));
-        quiz.add(new Quiz("Muralha de Cristal e Extinção são grandes golpes de qual Cavaleiro de Ouro?", "Mu de Áries", new String[]{" Aiolia de Leão"," Máscara da Morte de Câncer"," Mu de Áries"}));
-        quiz.add(new Quiz("O Cavaleiro de qual constelação usou um arco para ferir Saori?", "Sagitário", new String[]{" Cancer"," Sagitário"," Libra"}));
-        quiz.add(new Quiz("Durante a corrida na batalha das Doze Casas, qual Cavaleiro de Ouro facilitou para Seiya e seus companheiros?", " Mu de Áries", new String[]{" Mu de Áries"," Saga de Gêmeos"," Shaka de Virgem"}));
-        quiz.add(new Quiz("Qual o nome do Deus do Sono?", " Hypnos", new String[]{" Hypnos"," Ártemis"," Thanatos"}));
-        quiz.add(new Quiz("Onde Hyoga de Cisne obteve a sua Armadura?", " Sibéria Oriental", new String[]{" Colinas Bomi, Libéria"," Montanhas Rochosas, Canadá"," Sibéria Oriental"}));
-        quiz.add(new Quiz("A armadura de qual constelação é um arsenal de armas?", " Libra", new String[]{" Libra"," Peixes"," Áries"}));
+        quiz.add(new Quiz("Qual cavaleiro ajudou Athena a escapar do Santuário há 13 anos?", "Sagitário", new String[]{"Sagitário","Libra","Aries"}));
+        quiz.add(new Quiz("Qual Cavaleiro de Ouro é o mais belo?", "Afrodite de Peixes", new String[]{"Shura de Capricórnio","Afrodite de Peixes","Aldebaran de Touro"}));
+        quiz.add(new Quiz("Quem é o Cavaleiro de Ouro da Casa de Leão?", "Aioria", new String[]{"Aioria","Miro","Camus"}));
+        quiz.add(new Quiz("Cólera do Dragão é um grande golpe de qual Cavaleiro?", "Shiryu", new String[]{"Dohko","Shion","Shiryu"}));
+        quiz.add(new Quiz("Muralha de Cristal e Extinção são grandes golpes de qual Cavaleiro de Ouro?", "Mu de Áries", new String[]{"Aiolia de Leão","Máscara da Morte de Câncer","Mu de Áries"}));
+        quiz.add(new Quiz("O Cavaleiro de qual constelação usou um arco para ferir Saori?", "Sagitário", new String[]{"Cancer","Sagitário","Libra"}));
+        quiz.add(new Quiz("Durante a corrida na batalha das Doze Casas, qual Cavaleiro de Ouro facilitou para Seiya e seus companheiros?", "Mu de Áries", new String[]{"Mu de Áries","Saga de Gêmeos","Shaka de Virgem"}));
+        quiz.add(new Quiz("Qual o nome do Deus do Sono?", "Hypnos", new String[]{"Hypnos","Ártemis","Thanatos"}));
+        quiz.add(new Quiz("Onde Hyoga de Cisne obteve a sua Armadura?", "Sibéria Oriental", new String[]{"Colinas Bomi,Libéria","Montanhas Rochosas,Canadá","Sibéria Oriental"}));
+        quiz.add(new Quiz("A armadura de qual constelação é um arsenal de armas?", "Libra", new String[]{"Libra","Peixes","Áries"}));
         
         Collections.shuffle(quiz);
         
